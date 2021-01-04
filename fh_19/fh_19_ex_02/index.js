468214307ab55994c33ad7688eafaaa2c21c3a75
kokik44 <kouki_315@icloud.com>
2021-01-04 16:44:21 +0900

    21_01_04

--- fh_19/fh_19_ex_02/index.js
+++ fh_19/fh_19_ex_02/index.js
@@ -3,5 +3,5 @@ const axios = require('axios');
 
 (async () => {
   const response = await axios.get('https://opentdb.com/api.php?amount=10&type=multiple');
-  console.log(response.data.results);
+  console.log(`クイズデータ :`, response.data.results);
 })();
